887deec763b93ec60ffcc6cb2583fa7b7a1e7c49
JC <you@example.com>
2024-07-24 08:30:12 -0400

    Add about outrage

--- createMD.js
+++ createMD.js
@@ -1,12 +1,16 @@
-import { v4 as uuid4 } from "uuid";
 import fs from "fs";
 import path from "path";
 
+if (process.argv.length < 3) {
+  console.warn(`id is not provided`);
+  console.warn(`Usage: yarn createMD [id]`);
+  process.exit(1);
+}
+
+const id = process.argv[2];
 const __dirname = path.resolve();
 const mdDir = path.resolve(__dirname, "./md");
 const ids = fs.readdirSync(mdDir);
-
-const id = uuid4();
 
 if (ids.includes(id)) {
   console.warn(`Cannot generate new id: ${id} exists in ${mdDir}`);
@@ -15,6 +19,14 @@ if (ids.includes(id)) {
 
 const newDir = path.resolve(mdDir, id);
 const newMain = path.resolve(newDir, "main.md");
+const newMainContent = `---
+title: ""
+createdAt: ""
+summary: ""
+---
+
+TODO
+`;
 
 fs.mkdirSync(newDir);
-fs.writeFileSync(newMain, "");
+fs.writeFileSync(newMain, newMainContent);
